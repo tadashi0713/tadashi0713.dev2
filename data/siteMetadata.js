ae0c8f7ac4bccf93883f96a5e842c74df4a68fcf
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2023-08-07 04:27:20 +0900

    sitemetadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,22 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Tadashi Nemoto',
+  author: 'Tadashi Nemoto',
+  headerTitle: 'Tadashi Nemoto',
+  description: 'Portfolio site of Tadashi Nemoto',
   language: 'en-us',
-  theme: 'system', // system, dark or light
+  theme: 'dark', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'tadashi.nemoto0713@gmail.com',
+  github: 'https://github.com/tadashi0713',
+  // twitter: 'https://twitter.com/Twitter',
+  facebook: 'https://www.facebook.com/nemoto.tadashi',
+  // youtube: 'https://youtube.com',
+  linkedin: 'https://www.linkedin.com/in/tadashi-nemoto',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
